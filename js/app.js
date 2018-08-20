049659289d930cb22613bb662a5a5d34185d856e
Bhaskar kumar <allstarbhaskarkumar@gmail.com>
2018-08-20 21:32:10 +0530

    feat: toggle switch with red & green color when its off and on

--- js/app.js
+++ js/app.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
     $("input").change(function() {
         if($(this).is(":checked")) {
             console.log("Is checked");
-            $(this).children().addClass('red');
+            $(this).siblings().addClass('green');
         }
         else {
             console.log("Is Not checked");
+            $(this).siblings().removeClass('green');
+            $(this).siblings().addClass('red');
         }
     })
 });
