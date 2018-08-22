617848af1e167a47d52b287b3fc8aa9b8793f785
Bhaskar kumar <allstarbhaskarkumar@gmail.com>
2018-08-23 01:39:36 +0530

    feat: Add toggle all switchs functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
+
+
+    function toggleMainSwitch(parentSwitchClass, childSwicheClass){
+        //Main toggle switch
+        $(parentSwitchClass).change(function() {
+            if($(this).is(":checked")) {
+                console.log("Is checked");
+                //Set all the child switches state to unblock
+                $(childSwicheClass).prop('checked', true);
+                $(childSwicheClass).siblings().addClass('green');
+                $(this).siblings().addClass('green');
+            }
+            else {
+                console.log("Is Not checked");
+                //Set all the child switches state to block
+                $(childSwicheClass).prop('checked', false);
+                $(childSwicheClass).siblings().removeClass('green');
+                $(this).siblings().removeClass('green');
+                $(this).siblings().addClass('red');
+            }
+        });
+    }
+    
+    toggleMainSwitch('.agPortsParent', '.agPortsChild');
+    toggleMainSwitch('.vnPortParent', '.vnPortChild');
+    toggleMainSwitch('.ngccPortParent', '.ngccPortChild');
+
+    //Individual toggle switch
     $("input").change(function() {
         if($(this).is(":checked")) {
             console.log("Is checked");
