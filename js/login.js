651196ccb67f11911c70fd288915118b5d7c2d91
Bhaskar kumar <allstarbhaskarkumar@gmail.com>
2018-12-11 12:03:31 +0530

    feat: go to home page after authentication

--- js/login.js
+++ js/login.js
@@ -4,4 +4,7 @@ $("#login-button").on('click', (e) => {
     const password = $("#password").val();
     console.log(`User name: ${username}, password: ${password}`);
     //TODO: Add authentication python script here
+
+    //Go to home page after authentication
+    window.location = './index.html';
 })
